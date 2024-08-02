35c080e573452333db6c124e500cbac6a7377b0e
Pavlo Zalutskiy <kyrogane1@gmail.com>
2024-08-02 11:35:52 +0300

    Commit

--- scripts/mintNft.ts
+++ scripts/mintNft.ts
@@ -1,25 +1,33 @@
 import hre, { ethers, network } from "hardhat";
-import { deployBasicNft } from "./deployBasicNft";
-import { deployRandomIpfsNft } from "./deployRandomIpfsNft";
-import { deployDynamicSvgNft } from "./deployDynamicSvgNft";
-import { developmentChains } from "../../helper-hardhat-config";
-import { type EventLog } from "ethers";
+import { type Contract } from "ethers";
+import { type RandomIpfsNft } from "typechain-types";
 
 async function main() {
-  const { name } = network;
   const { chainId } = network.config;
 
   if (!chainId) return console.log("Chain id isn't detected!");
 
   await hre.run("compile");
-  const basicNft = await deployBasicNft(name);
-  const { randomIpfsNft, vrfCoordinatorV2_5Mock } = await deployRandomIpfsNft(name, chainId);
-  const dynamicSvgNft = await deployDynamicSvgNft(name, chainId);
+  const basicNft = await ethers.getContractAt(
+    "BasicNft",
+    "0xb7b93044a12Fb4905640C8D36da586816f9E1f51",
+  );
 
-  // // Basic NFT
+  const randomIpfsNft = (await ethers.getContractAt(
+    "RandomIpfsNft",
+    "0x54dcaEfaD9558040A8eb895B03B620305904eb8F",
+  )) as Contract & RandomIpfsNft;
+
+  const dynamicSvgNft = await ethers.getContractAt(
+    "DynamicSvgNft",
+    "0x85Da80684c1E8A3DEb99D4207cC2302a9657B07F",
+  );
+
+  // Basic NFT
   const basicNftMintTx = await basicNft.mintNft();
   await basicNftMintTx.wait(1);
   console.log(`Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`);
+  //https://sepolia.etherscan.io/address/0xb7b93044a12Fb4905640C8D36da586816f9E1f51#code
 
   // Random Ipfs NFT
   const mintFee = await randomIpfsNft.getMintFee();
@@ -31,22 +39,18 @@ async function main() {
     });
 
     const randomIpfsNftMintTx = await randomIpfsNft.requestNft({ value: String(mintFee) });
-    const randomIpfsNftMintTxReceipt = await randomIpfsNftMintTx.wait(1);
-
-    if (developmentChains.includes(network.name)) {
-      const [requestId] = (randomIpfsNftMintTxReceipt?.logs[1] as EventLog).args;
-      const randomIpfsNftAddress = await randomIpfsNft.getAddress();
-      await vrfCoordinatorV2_5Mock!.fulfillRandomWords(requestId, randomIpfsNftAddress);
-    }
+    await randomIpfsNftMintTx.wait(1);
   });
 
   console.log(`Random Ipfs NFT index 0 has tokenURI: ${await randomIpfsNft.tokenURI(0)}`);
+  // https://sepolia.etherscan.io/address/0x54dcaEfaD9558040A8eb895B03B620305904eb8F#code
 
   // Dynamic Svg NFT
   const highValue = ethers.parseEther("3500");
   const dynamicSvgNftMintTx = await dynamicSvgNft.mintNft(highValue);
   await dynamicSvgNftMintTx.wait(1);
   console.log(`Dynamic Svg NFT index 0 has tokenURI: ${await dynamicSvgNft.tokenURI(0)}`);
+  // https://sepolia.etherscan.io/address/0x85Da80684c1E8A3DEb99D4207cC2302a9657B07F#code
 }
 
 main()
